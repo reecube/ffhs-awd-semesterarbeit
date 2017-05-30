4a12665563b47f0cc341ac3b60005ca61e554e6b
Yves Riedener <git@reecube.com>
2017-05-30 16:48:04 +0200

    added part 5

---
+++ 5/code/test.py
@@ -0,0 +1,16 @@
+##################################################
+#
+# Aufgabenstellung
+#
+# Implementieren Sie die Trapezregel und die Simpsonsche Regel zur numerischen Bestimmung von bestimmten Integralen.
+#
+# Wenden Sie diese Algorithmen auf die Berechnung des Volumens eines Kreisrings an.
+#   (Ein Kreisring kann als Rotationskörper eines Kreises aufgefasst werden,
+#   wenn der Kreis sich vollständig oberhalb der xx-Achse befindet und wenn er um die xx-Achse rotiert wird.
+#
+# Vergleichen Sie die Genauigkeit dieser Approximationen miteinander und
+#   vergleichen Sie die Resultate mit den exakten Werten, die sie von Hand und mit mit Sympy bestimmen.
+#
+# Schließen Sie Ihre Semesterarbeit mit einer Diskussion des Einsatzes von Python für praxisbezogene Anwendungen ab.
+#
+##################################################
