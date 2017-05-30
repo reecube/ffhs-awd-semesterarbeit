45ca89dda7f9e0a530f33483e69d388177233e25
Yves Riedener <git@reecube.com>
2017-05-30 16:47:54 +0200

    added part 4b

---
+++ 4b/code/test.py
@@ -0,0 +1,13 @@
+##################################################
+#
+# Aufgabenstellung
+#
+# Geben Sie einige Beispiele, wie Funktionen mit Matplotlib visualisiert werden können.
+#
+# Schauen Sie sich dazu z.B. folgende Tutorials an:
+# - multidimensionale Splines:
+#     http://docs.scipy.org/doc/scipy/reference/tutorial/interpolate.html
+# - 3D Grafiken von 2-dimensionalen Funktionen
+#     http://matplotlib.org/examples/mplot3d/surface3d_demo3.html
+#
+##################################################
