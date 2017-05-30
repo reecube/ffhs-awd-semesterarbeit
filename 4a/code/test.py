0c318a7f44b58adff6ee7c5c43aed334ad530686
Yves Riedener <git@reecube.com>
2017-05-30 16:49:09 +0200

    copy paste issue

--- 4a/code/test.py
+++ 4a/code/test.py
@@ -2,8 +2,8 @@
 #
 # Aufgabenstellung
 #
-# Visualisieren Sie die Approximation einer Funktion durch Taylorpolynome (z.B. von sin(x)sin⁡(x)
-#   oder von ln(x+1)ln⁡(x+1). Schreibem Sie eine Python-Funktion, die die Graphen der Ursprungsfunktion
+# Visualisieren Sie die Approximation einer Funktion durch Taylorpolynome (z.B. von sin(x)
+#   oder von ln(x+1). Schreibem Sie eine Python-Funktion, die die Graphen der Ursprungsfunktion
 #   sowie der Taylorpolynome bis zu einem bestimmten Grad mit Matplotlib visualisiert.
 #
 ##################################################
