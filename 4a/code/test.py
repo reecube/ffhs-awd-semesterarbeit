08765a18cdda260032b0cbc611ff2fa19ebecf5b
Yves Riedener <git@reecube.com>
2017-06-02 11:06:44 +0200

    improved code and added comments

--- 4a/code/test.py
+++ 4a/code/test.py
@@ -8,61 +8,76 @@
 #
 ##################################################
 
+# Libraries importieren
 import sympy
 from sympy import lambdify
 import tools
 import numpy as np
 import matplotlib.pyplot as plt
 
+# X-Symbol für die Funktionen erstellen
 x = sympy.Symbol('x')
 
 
+# Zeigt eine Funktion mit Anzahl Taylor-Polynomen an auf der angegebenen Feldgrösse
 def show_taylor_graph(fnc, depth, grid_min, grid_max):
     print('Die Taylorpolynome für', fnc, 'werden berechnet und angezeigt:')
 
+    # Definiert die Feldgrösse für den Graphen
     x_lims = [grid_min, grid_max]
+    y_lims = [grid_min, grid_max]
+    plt.xlim(x_lims)
+    plt.ylim(y_lims)
+
+    # Erstellt 800 regelmässige x1-Werte zwischen Feldgrösse-Minimum und Feldgrösse-Maximum
     x1 = np.linspace(x_lims[0], x_lims[1], 800)
     y1 = []
 
+    #
     print('')
     print('berechnen...')
-    for i in range(1, depth + 1, 1):
-        func = tools.taylor_series(fnc, 0, i * 2)
+    for n in range(1, depth + 1, 1):
+        func = tools.taylor_series(fnc, 0, n)
 
         for k in x1:
             y1.append(func.subs(x, k))
 
-        plt.plot(x1, y1, label='n=' + str(i))
+        plt.plot(x1, y1, label='n=' + str(n))
 
         y1 = []
 
-        print('n=' + str(i), func)
+        print('n=' + str(n), func)
 
+    # Erstellt die Werte für die Basis-Funktion
     lam_base = lambdify(x, fnc, modules=['numpy'])
     plt.plot(x1, lam_base(x1), label=str(fnc))
 
-    plt.xlim(x_lims)
-    plt.ylim([grid_min, grid_max])
+    # Benennt die x und y Etiketten des Grpahen
     plt.xlabel('x')
     plt.ylabel('y')
 
+    # Zeigt im Graphen eine Legende an
     plt.legend()
 
+    # Zeigt im Graphen die Gitterlinien an
     plt.grid(True)
 
+    # Zeigt im Graphen einen Titel an
     plt.title('Approximation einer Funktion durch Taylorpolynome')
 
+    # Zeigt den Graphen an
     print('')
     print('anzeigen...')
     plt.show()
 
 
+# Die Demo-Funktionen welche angezeigt werden können
 demoFunctions = [
     sympy.sin(x),
     sympy.ln(x + 1),
 ]
 
-# Show valid functions
+# Zeigt die verfügbaren Funktionen welche angezeigt werden können
 print('')
 print('Funktionen:')
 print('-------------')
@@ -70,24 +85,26 @@ for (i, demoFunction) in enumerate(demoFunctions):
     print('`' + str(i) + '`:', demoFunction)
 print('')
 
-# Ask the user for a function
+# Frag den Benutzer welche Funktion er anzeigen möchte
 userInput = input('Bitte wählen Sie einen Ausgabetypen: ')
 
 print('')
 
+# Versucht die Benutzereingabe in eine Zahl umzuwandeln
 try:
     selectedFunctionIdx = int(userInput)
 except ValueError:
     selectedFunctionIdx = -1
 
+# Zeigt die ausgewählte Funktion an, falls diese gültig ist
 if 0 <= selectedFunctionIdx < len(demoFunctions):
-    depth = 5
-    grid_min = -5
-    grid_max = 5
+    val_depth = 5
+    val_grid_min = -5
+    val_grid_max = 5
 
     if selectedFunctionIdx == 1:
-        grid_min = 0
+        val_grid_min = 0
 
-    show_taylor_graph(demoFunctions[selectedFunctionIdx], depth, grid_min, grid_max)
+    show_taylor_graph(demoFunctions[selectedFunctionIdx], val_depth, val_grid_min, val_grid_max)
 else:
     print('Die Eingabe `' + userInput + '` ist ungültig!')
--- 4a/code/tools.py
+++ 4a/code/tools.py
@@ -1,8 +1,10 @@
 import sympy
 
+# X-Symbol für die Funktionen erstellen
 x = sympy.Symbol('x')
 
 
+# Berechnet die Fakultät, also n! wenn n <= 0 dann wird 1 zurückgegeben.
 def factor(n):
     if n <= 0:
         return 1
@@ -10,10 +12,17 @@ def factor(n):
         return n * factor(n - 1)
 
 
+# Berechnet die Taylor-Polynome1
+#   fnc:  Ist die Funktion für das Taylor-Polynom
+#   x0:   Ist der Startwert
+#   n:    Ist der Grad für das Taylor-Polynom
 def taylor_series(fnc, x0, n):
-    i = 0
+    # Standardwert und Startwert für p ist 0
     p = 0
-    while i <= n:
+
+    for i in range(0, n + 1):
+        print('for  ', i, p)
         p = p + (fnc.diff(x, i).subs(x, x0)) / (factor(i)) * (x - x0) ** i
-        i += 1
+
+    # Nun wird p zurückgegeben, falls n >= 0 wird p eine Funktion sein
     return p
